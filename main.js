c02cb3892bad63485ed26f48bb553fb0a60b80f5
ecowhite <ecowhite.ac.id@gmail.com>
2023-10-20 13:45:35 +0700

    menambahkan fitur contak agar berfungsi

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzxVE6tpczh_CA54A-nwuSoC1LfirNHKCRBUGAGl2dJa2OI4g_-rTQmRpvP5g0vnUuG/exec'
+const form = document.forms['ecowhite-contack-form']
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const form = document.querySelector('form');
+const btnSend = document.querySelector('.btn-send')
+const btnLoading = document.querySelector('.btn-loading')
+const myAlert = document.querySelector('.my-alert')
 
-        form.addEventListener('submit', function (event) {
-            event.preventDefault(); // Menghentikan pengiriman formulir
 
-            // Di sini Anda dapat menambahkan kode untuk mengirim data formulir ke server jika diperlukan
-
-            // Menampilkan pesan alert
-            alert('Pesan Anda telah terkirim!');
-
-            // Atau Anda juga dapat menggunakan window.alert()
-            // window.alert('Pesan Anda telah terkirim!');
-        });
-    });
-
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // ketika tombol submit diklick
+    // tampilkan tombol loading, hilangkan tombol kirim
+    btnLoading.classList.toggle('d-none')
+    btnSend.classList.toggle('d-none')
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            // ketika tombol submit diklick
+            // tampilkan tombol kirm, hilangkan tombol loading
+            btnLoading.classList.toggle('d-none')
+            btnSend.classList.toggle('d-none')
+            // tampilkan alert
+            myAlert.classList.toggle('d-none')
+            // reset form isi
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
